fa570712e617c4e20a1fb773304736899000ed01
Marton Zugn <mzugn@protonmail.ch>
2024-12-19 10:22:34 +0100

    refactor 19 part 2

--- Day19b.fs
+++ Day19b.fs
@@ -6,35 +6,19 @@ open AdventOfCode2024.Day19a
 let isDesignPossibleWithPermutationCount (patterns: string []) (design: string) =
     let cache = Dictionary<string, bool * uint64>()
 
-    let rec isPossible (design: string) (numberOfWaysToGetHere: uint64) =
+    let rec isPossible (design: string) (waysToGetHere: uint64) =
         match cache.TryGetValue design with
-        | true, (possible, waysToAchieve) -> possible, numberOfWaysToGetHere * waysToAchieve
+        | true, (possible, waysToAchieve) -> possible, waysToGetHere * waysToAchieve
         | false, _ ->
             let result =
                 if design.Length = 0 then
-                    true, numberOfWaysToGetHere
+                    true, waysToGetHere
                 else
-                    let matchingPatterns =
-                        patterns
-                        |> Seq.filter (fun p -> design.StartsWith(p))
-                        |> Seq.map (fun p -> p.Length)
-                        |> Seq.countBy id
-                        |> Seq.toList
-
-                    if matchingPatterns.IsEmpty then
-                        false, 0UL
-                    else
-                        let subResults =
-                            matchingPatterns
-                            |> List.map (fun (patternLength, count) ->
-                                let remainingDesign = design.Substring(patternLength)
-                                isPossible remainingDesign (numberOfWaysToGetHere * uint64 count))
-                            |> List.filter fst
-
-                        if subResults.IsEmpty then
-                            false, 0UL
-                        else
-                            true, (subResults |> List.sumBy snd)
+                    patterns
+                    |> Seq.filter (fun p -> design.StartsWith(p))
+                    |> Seq.map (fun p -> isPossible (design.Substring(p.Length)) (waysToGetHere * 1UL))
+                    |> Seq.filter fst
+                    |> Seq.fold (fun (_, total) (_, count) -> (true, total + count)) (false, 0UL)
 
             cache.Add(design, result)
             result
